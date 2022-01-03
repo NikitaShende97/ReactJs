01fcd5da1415b3d11974f5b054236d942d596a13
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-03 15:56:39 +0530

    b8

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import like from "./onclick.js";
+import dislike from "./onclick.js";
 
 export default function App() {
-  function like(param) {
-    // let prev = param.ParentElement.children[1];
-  }
-  function dislike(param) {}
+  let list = [1, 1, 1, 1, 1, 1];
   return (
     <div className="container-fluid">
       <div className="row bg-dark text-white sticky-top">
@@ -12,89 +10,37 @@ export default function App() {
           React Example
         </div>
       </div>
-      <div className="row bg-warning" style={{ height: "100vh" }}>
+      <div className="row bg-warning" style={{ height: "100%" }}>
         <div className="row justify-content-center align-items-center">
-          <div className="col-3 m-1">
-            <div className="card bg-dark text-white ">
-              <div className="card-header">Images</div>
-              <div className="card-body mx-0 px-0 py-0">
-                <img
-                  src="https://picsum.photos/200"
-                  style={{ width: "100%" }}
-                ></img>
-                <div className="mt-2 mx-2">
-                  Lorem Alternatively, you can use this shorthand version for
-                  Cards with body only, and no other children
-                </div>
-                <div className="mx-2">
-                  <span onClick={like} style={{ cursor: "pointer" }}>
-                    &#128077;
-                  </span>{" "}
-                  &nbsp;
-                  <span>0</span>&nbsp;&nbsp;
-                  <span onClick={dislike(this)} style={{ cursor: "pointer" }}>
-                    &#128078;
-                  </span>
-                  &nbsp;&nbsp;
-                  <span>0</span>&nbsp;
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-3 m-1">
-            <div className="card bg-dark text-white">
-              <div className="card-header">Images</div>
-              <div className="card-body mx-0 px-0 py-0">
-                <img
-                  src="https://picsum.photos/200"
-                  style={{ width: "100%" }}
-                ></img>
-                <div className="mt-2 mx-2">
-                  Lorem Alternatively, you can use this shorthand version for
-                  Cards with body only, and no other children
-                </div>
-                <div className="mx-2">
-                  <span onClick={like(this)} style={{ cursor: "pointer" }}>
-                    &#128077;
-                  </span>{" "}
-                  &nbsp;
-                  <span>0</span>&nbsp;&nbsp;
-                  <span onClick={dislike(this)} style={{ cursor: "pointer" }}>
-                    &#128078;
-                  </span>
-                  &nbsp;&nbsp;
-                  <span>0</span>&nbsp;
+          {list.map((item) => (
+            <div className="col-3 m-1">
+              <div className="card bg-dark text-white ">
+                <div className="card-header">Images</div>
+                <div className="card-body mx-0 px-0 py-0">
+                  <img
+                    src="https://picsum.photos/200"
+                    style={{ width: "100%" }}
+                  ></img>
+                  <div className="mt-2 mx-2">
+                    Lorem Alternatively, you can use this shorthand version for
+                    Cards with body only, and no other children
+                  </div>
+                  <div className="mx-2">
+                    <span onClick={like} style={{ cursor: "pointer" }}>
+                      &#128077;
+                    </span>{" "}
+                    &nbsp;
+                    <span>0</span>&nbsp;&nbsp;
+                    <span onClick={dislike} style={{ cursor: "pointer" }}>
+                      &#128078;
+                    </span>
+                    &nbsp;&nbsp;
+                    <span>0</span>&nbsp;
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-3 m-1">
-            <div className="card bg-dark text-white">
-              <div className="card-header">Images</div>
-              <div className="card-body mx-0 px-0 py-0">
-                <img
-                  src="https://picsum.photos/200"
-                  style={{ width: "100%" }}
-                ></img>
-                <div className="mt-2 mx-2">
-                  Lorem Alternatively, you can use this shorthand version for
-                  Cards with body only, and no other children
-                </div>
-                <div className="mx-2">
-                  <span onClick={like(this)} style={{ cursor: "pointer" }}>
-                    &#128077;
-                  </span>
-                  &nbsp;
-                  <span>0</span>&nbsp;&nbsp;
-                  <span onClick={dislike(this)} style={{ cursor: "pointer" }}>
-                    &#128078;
-                  </span>
-                  &nbsp;&nbsp;
-                  <span>0</span>&nbsp;
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
--- src/onclick.js
+++ src/onclick.js
@@ -5,4 +5,12 @@ function like(param) {
   prev.innerHTML = newVal;
   console.log(newVal);
 }
-export default like;
+
+function dislike(param) {
+  let prev = param.parentElement.children[1];
+  let preVal = prev.innerHTML;
+  let newVal = parseInt(preVal) + 1;
+  prev.innerHTML = newVal;
+  console.log(newVal);
+}
+export default { like, dislike };
