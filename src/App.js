3e2116a74ffe7d82d89b14e94266d89172a242e9
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-03 14:34:12 +0530

    b6

--- src/App.js
+++ src/App.js
@@ -6,9 +6,45 @@ export default function App() {
         className="row bg-dark "
         style={{ backgroundColor: "black", height: "100vh" }}
       >
-        <div className="d-flex justify-content-center align-items-center">
-          <img src={download} alt="" height={250} width={200}></img>
-          <h1 className="text-white">React Js</h1>
+        <div className="col d-flex justify-content-center align-items-center flex-column">
+          <div className="row">
+            <div className="col">
+              <label for="email" className="text-white">
+                Email
+              </label>
+              <input
+                type="text"
+                placeholder="abc@gmail.com"
+                className="form-control"
+              ></input>
+            </div>
+          </div>
+          <div className="row mt-1">
+            <div className="col">
+              <label for="email" className="text-white">
+                Password
+              </label>
+              <input
+                type="password"
+                placeholder=""
+                className="form-control"
+              ></input>
+            </div>
+          </div>
+          <div className="row mt-1">
+            <div className="col">
+              <input
+                type="button"
+                value="Login"
+                className="btn btn-secondary"
+              ></input>
+              <input
+                type="button"
+                value="Reset"
+                className="btn btn-secondary mx-2"
+              ></input>
+            </div>
+          </div>
         </div>
       </div>
     </div>
